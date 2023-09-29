037c4949c501d06e7bfee1c4f8a55d2ee894fa12
dhoorn <s1159965@student.windesheim.nl>
2023-09-29 12:30:28 +0200

    succesfully read csv file and send over usb

---
+++ Project1/SerSendBoxContents.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import serial
+import threading
+
+serial_port = 'COM18'
+
+# Function to be called when the button is clicked
+def button_click():
+    x1 = entry1.get()
+    threading.Thread(target=send_and_receive_data, args=(x1,)).start()
+
+
+def serial_open():
+    global ser
+    ser = serial.Serial(serial_port, baudrate=9600)
+
+def serial_close():
+    ser.close()
+
+# Function to send and receive data in a separate thread
+def send_and_receive_data(x1):
+    try:
+        print(2)
+        ser.write(x1.encode())
+        print(3)
+        x2 = ser.read(len(x1))
+        print(4)
+        label.config(text=x2.decode())  # Decode received bytes to string
+        print(5)
+    except serial.SerialException as e:
+        label.config(text=f"Error: {str(e)}")
+
+# Create the main window
+root = tk.Tk()
+root.title("Larger GUI")
+root.geometry("1920x1080")  # Set the initial window size to 1920x1080 pixels
+
+# Create a label
+label = tk.Label(root, text="FPGA TEST GUI")
+label.pack(pady=20)
+
+# Create an input box
+entry1 = tk.Entry(root)
+entry1.pack()
+
+# Create a button
+button = tk.Button(root, text="Send!", command=button_click)
+button.pack()
+
+# Create a button
+button = tk.Button(root, text="Open serial", command=serial_open)
+button.pack()
+
+# Create a button
+button = tk.Button(root, text="Close serial", command=serial_close)
+button.pack()
+
+# Start the main loop
+root.mainloop()
--- Project1/main.py
+++ Project1/main.py
@@ -1,41 +1,63 @@
 import tkinter as tk
+from tkinter import filedialog
 import serial
 import threading
+import csv
 
 serial_port = 'COM18'
 
 # Function to be called when the button is clicked
 def button_click():
-    x1 = entry1.get()
-    threading.Thread(target=send_and_receive_data, args=(x1,)).start()
+    threading.Thread(target=send_and_receive_data).start()
+
+def serial_open():
+    global ser
+    ser = serial.Serial(serial_port, baudrate=9600)
+
+def serial_close():
+    ser.close()
+
+def selectFile():
+   global file_path
+   file_path = filedialog.askopenfilename()
 
 # Function to send and receive data in a separate thread
-def send_and_receive_data(x1):
+def send_and_receive_data():
     try:
-        ser = serial.Serial(serial_port, baudrate=9600)
-        message = x1
-        ser.write(message.encode())
-        x2 = ser.read(100)
-        label.config(text=x2.decode())  # Decode received bytes to string
-        ser.close()
+        with open(file_path) as csv_file:
+            
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            for row in csv_reader:
+                ser.write(row[0].encode())
+                response = ser.read(len(row[0]))
+                #response_decoded = response.decode('utf-8')
+                print(response)
     except serial.SerialException as e:
-        label.config(text=f"Error: {str(e)}")
+        label.config(text=f"Error {str(e)}")
 
 # Create the main window
 root = tk.Tk()
-root.title("Larger GUI")
-root.geometry("1920x1080")  # Set the initial window size to 1920x1080 pixels
+root.title("Python test")
+root.geometry("300x200")  # Set the initial window size to 1920x1080 pixels
 
 # Create a label
 label = tk.Label(root, text="FPGA TEST GUI")
 label.pack(pady=20)
 
-# Create an input box
-entry1 = tk.Entry(root)
-entry1.pack()
-
 # Create a button
 button = tk.Button(root, text="Send!", command=button_click)
+button.pack()
+
+# Create file selection button
+button = tk.Button(root, text="Select File", command=selectFile)
+button.pack()
+
+# Create serial open button
+button = tk.Button(root, text="Open Serial", command=serial_open)
+button.pack()
+
+# Create file selection button
+button = tk.Button(root, text="Serial Close", command=serial_close)
 button.pack()
 
 # Start the main loop
