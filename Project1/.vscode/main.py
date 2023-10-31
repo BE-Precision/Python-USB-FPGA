a7e626c2433315457268b0091c7f1da52c1698e2
xLuke11 <112888051+xLuke11@users.noreply.github.com>
2023-10-31 18:24:58 +0100

    Except  IndexError

--- Project1/.vscode/main.py
+++ Project1/.vscode/main.py
@@ -103,7 +103,7 @@ def save_parameters_to_json():
     }
     save_button.config(state=tk.DISABLED)
     try:
-        with open(".vscode\settings.json", "w") as json_file:
+        with open("Project1\.vscode\settings.json", "w") as json_file:
             json.dump(parameters, json_file, indent=4)
         messagebox.showinfo("Settings are saved", "Settings are saved")
     except Exception as e:
@@ -114,7 +114,7 @@ def load_parameters_from_json():
     global grid_columns, group_size, modules, square_size, COLORS, width, height
 
     try:
-        with open(".vscode\settings.json", "r") as json_file:
+        with open("Project1\.vscode\settings.json", "r") as json_file:
             parameters = json.load(json_file)
             grid_columns = parameters.get("grid_columns", grid_columns)
             group_size = parameters.get("group_size", group_size)
@@ -317,7 +317,9 @@ def send_and_receive_data():
         update_square_colors()
         updateList.clear()  # Wis de lijst met update-vierkanten
         colourList.clear()  # Wis de lijst met kleuren
-
+    except IndexError:
+        messagebox.showerror("Error", f"COM Port for module {moduleNumber} already used")
+        log_message(f"Error: COM Port for module {moduleNumber} already used")
     except FileNotFoundError:
         messagebox.showerror("Error", "The selected file doesn't exist.")
         log_message("Error: The selected file doesn't exist.")
@@ -385,7 +387,7 @@ def switch_to_screen2():
 # Create the main window
 root = tk.Tk()
 root.title("BE Precision Technology - Probe Card Tester")
-root.iconbitmap(".vscode\BEPTLogo.ico")
+root.iconbitmap("Project1\.vscode\BEPTLogo.ico")
 root.geometry(f"{width}x{height}")  # Set the initial window size to 1920x1080 pixels
 root.configure(bg="white")
 
