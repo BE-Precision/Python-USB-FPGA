e32ba54990dca92e76fd446fa1f7524e75d71896
Dannick-Hoorn <112714093+Dannick-Hoorn@users.noreply.github.com>
2023-11-12 09:48:24 +0100

    added script for generating FPGA code

---
+++ Project1/.vscode/Misc Scripts/FPGACGC.py
@@ -0,0 +1,13 @@
+with open('generatedcode.txt', 'w') as f:
+    for i in range(50):
+        s = i*2
+        f.write(f'{i}:')
+        f.write('\n\t')
+        f.write(f'if (signalSel.q == 0){{iopin[{s}] = 0; iopin[{s+1}] = 0;}}')
+        f.write('\n\t')
+        f.write(f'if (signalSel.q == 1){{iopin[{s}] = 0; iopin[{s+1}] = 1;}}')
+        f.write('\n\t')
+        f.write(f'if (signalSel.q == 2){{iopin[{s}] = 1; iopin[{s+1}] = 0;}}')
+        f.write('\n\t')
+        f.write(f'if (signalSel.q == 3){{iopin[{s}] = 1; iopin[{s+1}] = 1;}}')
+        f.write('\n')
